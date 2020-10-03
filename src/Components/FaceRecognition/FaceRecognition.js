00d9b2553162621893ec61305f2b972b77e4a95a
majewski_piotr <majewski.piotr.511@gmail.com>
2020-10-03 16:31:25 +0200

    added responsivnes to boundary box

--- src/Components/FaceRecognition/FaceRecognition.js
+++ src/Components/FaceRecognition/FaceRecognition.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import './FaceRecognition.css'
 
-const FaceRecognition =({ image })=>{
+const FaceRecognition =({ image, faceBoxArray })=>{
     return(
         <div className='center ma'>
-            <div className='absolute mt2'styles={{maxWidth:'70%', maxHeight:'auto'}}>
-                <img alt='maximum length of link is 200 characters.' src={image} />
+            <div className='absolute mt2'styles={{
+                maxWidth:'70%', maxHeight:'auto'
+                }}>
+                <img id='inputimage' alt='maximum length of link is 200 characters.' src={image} />
+                <div className='bounding-boxes' style={{
+                    position: 'absolute',
+                    left: faceBoxArray.left,
+                    top: faceBoxArray.top, 
+                    width: faceBoxArray.width,
+                    height: faceBoxArray.height,
+                    }}></div>
             </div>
+            
         </div>
     );
 }
